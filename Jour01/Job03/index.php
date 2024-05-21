1b387b392c58dfa53f40d023fdb87be16ae547de
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-05-21 16:22:28 +0200

    Jour01_Job03
    DLB_Jour01_Jour 03

--- Jour01/Job03/index.php
+++ Jour01/Job03/index.php
@@ -1,1 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Tableau html - php </title>
+</head>
+<body>
 <?php
+$col1 = 'Types';
+$col2 = 'nom';
+$col3 = 'value';
+$table = array('cat','col2','col3');
+?>
+
+<table>
+<tr>
+<?php echo"<td>$col1</td><td>$col2</td><td>$col3</td>";?>
+</tr>
+
+<tr>
+<?php echo"<td>$table[0]</td><td>$table[1]</td><td>$table[2]</td>";?>
+</tr>
+
+</table>
+
+
+</body>
+</html>
