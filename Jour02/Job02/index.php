bb672125c9d76ef07c8da84fb023b4a006e24c53
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-05-22 14:03:52 +0200

    Jour02_Job02
    Jour02_Job02

---
+++ Jour02/Job02/index.php
@@ -0,0 +1,10 @@
+<?php
+for ($num = 0 ; $num <= 1337 ; $num++) {
+    if ($num == 26 || $num== 37 || $num== 58 || $num== 88 || $num==1111 || $num== 3233) {
+        echo " " ."<br />";
+    } 
+    else {
+        echo $num ."<br />";
+    }
+
+}
