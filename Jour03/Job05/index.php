35fe1d5aed8883ac250648dd3b0fd4df864ef0a5
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-05-24 17:23:58 +0200

    Jour03_Job05

---
+++ Jour03/Job05/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour03_Job05</title>
+</head>
+<body>
+<table>
+<td>
+
+
+</td>
+
+
+</table>    
+
+
+</body>
+</html>
+
+<?php
+$textStr = "On n’est pas le meilleur quand on le croit mais quand on le sait." ;
+
+$dic
