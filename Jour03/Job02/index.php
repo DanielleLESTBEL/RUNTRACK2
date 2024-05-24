c06c85cc3552c77be07159b3564f5f87a7965749
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-05-24 14:30:49 +0200

    Jour03_Job02

---
+++ Jour03/Job02/index.php
@@ -0,0 +1,17 @@
+<?php
+$textStrg = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
+echo ($textStrg);
+
+for ($counter= 0; isset($textStrg[$counter]); $counter++) {
+ //   echo ($counter). "<br>"; 
+
+    if ($counter % 2 == 0) {
+        echo ($textStrg[$counter]);
+    }
+    else {
+        echo (" ");
+}
+}
+
+?>
+
