7271e0b09641318ccc234170aac61bac6ce58a30
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-05-28 15:36:16 +0200

    Jour04_Job02_03_04
    Modification des titres

--- Jour04/Job03/index.php
+++ Jour04/Job03/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jour04_Job01</title>
+    <title>Jour04_Job03</title>
 
 </head>
 <body>
