fc34bfeaf5107f29f4fa8cfdf3a9f4e49f2d9acc
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-05-28 14:19:25 +0200

    Jour04_Job02_04

--- Jour04/Job02/index.php
+++ Jour04/Job02/index.php
@@ -6,6 +6,11 @@
     <title>Jour04_Job01</title>
 
 </head>
+<style>
+    td, th{ 
+        border: 1px solid black;
+    }
+    </style>
 <body>
 <form action="index.php" method="GET"> 
     <label for="firstname">Prénom</label> 
@@ -32,6 +37,8 @@ if (isset($_GET['firstname']) && isset($_GET['lastname'])) {
 </thead>
 <td><?php echo $_GET['firstname']?></td>
 <td> <?php echo $_GET['lastname']?> </td>
+
+
 
 </table>
 
--- Jour04/Job04/index.php
+++ Jour04/Job04/index.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jour04_Job01</title>
 
+    <style>
+    td, th{ 
+        border: 1px solid black;
+    }
+    </style>
 </head>
 <body>
 <form action="index.php" method="POST"> 
