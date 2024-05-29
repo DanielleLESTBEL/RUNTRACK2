4f4dd0befb0e92cce75739897f445bd838937f42
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-05-29 14:53:27 +0200

    Jour07_Job1

---
+++ Jour07/Job01/index.php
@@ -0,0 +1,17 @@
+<?php 
+function hello() {
+    echo "Hello";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour07 Job01</title>
+</head>
+<body>
+   <?php hello(); ?> 
+</body>
+</html>
