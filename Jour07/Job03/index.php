d5ffd8269e5cea1d4bafb07c024721b6aca196b4
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-05-29 14:53:48 +0200

    Jour07_Job03

---
+++ Jour07/Job03/index.php
@@ -0,0 +1,17 @@
+<?php 
+function getHello() {
+    return "Hello LaPlateforme !";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour07 Job03</title>
+</head>
+<body>
+   <?php echo getHello(); ?> 
+</body>
+</html>
