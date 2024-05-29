26880ed8248e51c14503e384dae9be2b41da0ade
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-05-29 14:53:00 +0200

    Jour04_&_05

---
+++ Jour04_&_05/Job02/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour04_Job02</title>
+
+</head>
+<style>
+    td, th{ 
+        border: 1px solid black;
+    }
+    </style>
+<body>
+<form action="index.php" method="GET"> 
+    <label for="firstname">Prénom</label> 
+    <input type="text" name="firstname" id="firstname">
+    <label for="lastname">Nom</label>
+    <input type="text" name="lastname" id="lastname">
+    <input type="submit" value="Confirmer">
+</form>
+<?php
+
+if (isset($_GET['firstname']) && isset($_GET['lastname'])) {
+    
+} 
+
+
+?>
+
+<table>
+<thead>
+    <tr>
+        <th>Argument</th>
+        <th>Valeur</th>
+    </tr>
+</thead>
+<td><?php echo $_GET['firstname']?></td>
+<td> <?php echo $_GET['lastname']?> </td>
+
+
+
+</table>
+
+</body>
+</html>
---
+++ Jour04_&_05/Job07/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour04_Job07</title>
+
+    
+</head>
+<body>
+<form action="index.php" method="POST"> 
+    <label for="username">Utilisateur</label> 
+    <input type="text" name="username" id="username">
+    <label for="password">Mot de Passe</label>
+    <input type="text" name="password" id="password">
+    <input type="submit" value="Confirmer">
+</form>
+<?php
+
+if ($_POST['username'] == "John" && $_POST['password'] == "Rambo") {
+    echo "C'est pas ma guerre";
+} 
+elseif ($_POST['username'] == "Max" && $_POST['password'] == "Mox") {
+    echo "T'as réussi !";
+}
+else {
+    echo "Votre pire cauchemar";
+}
+
+?>
+
+</body>
+</html>
