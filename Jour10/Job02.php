2ba1b1374d47de367ce6039aff179552ca7c3919
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-06-05 13:55:23 +0200

    Jour10_Job02

---
+++ Jour10/Job02.php
@@ -0,0 +1,30 @@
+<?php
+$mysqli = mysqli_connect("localhost", "root", "", "jour09");
+$result = mysqli_query($mysqli, "SELECT nom, capacite  FROM salles");
+$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo "<table>";
+echo "<tr>
+        <th>nom</th>
+        <th>capacite</th>
+    </tr>";
+foreach ($rows as $row) {
+
+echo "<tr><th>{$row['nom']}</th><th>{$row['capacite']}</th></tr>";
+}
+echo "</table>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>runtrack2/jour10/job02</title>
+    <META TYPE="AUTHOR" VALUE="runtrack2/jour10/job02"/>
+
+</head>
+<body>
+
+
+</body>
+</html>
