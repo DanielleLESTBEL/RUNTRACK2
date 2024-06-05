c78389886914c38f570dd14ab26115e2381e0952
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-06-05 16:27:55 +0200

    Jour10_Job05

---
+++ Jour10/Job05/index.php
@@ -0,0 +1,38 @@
+<?php
+$mysqli = mysqli_connect("localhost", "root", "", "Jour09");
+$result = mysqli_query($mysqli, "SELECT prenom, nom, naissance, sexe, email FROM etudiants WHERE naissance >= DATE_SUB(CURDATE(), INTERVAL 18 YEAR)");
+$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo "<table>";
+echo "<tr>
+        <th> prenom </th>
+        <th> nom </th>
+        <th> naissance </th>
+        <th> sexe </th>
+        <th> email </th>
+    </tr>";
+
+foreach ($rows as $row) {
+echo "<tr>
+        <th>{$row['prenom']}</th>
+        <th>{$row['nom']}</th>
+        <th>{$row['naissance']}</th>
+        <th>{$row['sexe']}</th>
+        <th>{$row['email']}</th>
+    </tr>";
+}
+echo "</table>";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jour10_Job05</title>
+    <link rel= "stylesheet" href="./index.css">
+</head>
+<body>
+    
+</body>
+</html>
