0865b06e8132b9bf87459c4539f269e11e4024f4
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-06-06 16:44:03 +0200

    Jour10_Job12

--- Jour10/Job12/index.php
+++ Jour10/Job12/index.php
@@ -1,29 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-
-</head>
-<body>
 <?php
 $mysqli = mysqli_connect("localhost", "root", "", "jour09");
-$result = mysqli_query($mysqli, "SELECT nom, prenom, naissance FROM `etudiants` WHERE `naissance` BETWEEN ('1998-01-01') AND ('2018-12-31');");
+$result = mysqli_query($mysqli, "SELECT nom, prenom, naissance FROM `etudiants` WHERE `naissance` BETWEEN ('1988-01-01') AND ('2018-12-31');");
 $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
 echo "<table>";
 echo "<tr>
-        <th>nom</th>
-        <th>prenom</th>
-        <th>naissance</th>
+        <th>Prénom</th>
+        <th>Nom</th>
+        <th>Naissance</th>
     </tr>";
 foreach ($rows as $row) {
 
 echo "<tr>
-        <th>{$row['nom']}</th>
         <th>{$row['prenom']}</th>
+        <th>{$row['nom']}</th>
         <th>{$row['naissance']}</th>
     </tr>";
 }
 echo "</table>";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Jour10_Job12</title>
+    <link rel= "stylesheet" href="./index.css">
+</head>
+<body>
+
 </body>
 </html>
