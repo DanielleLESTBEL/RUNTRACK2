6f74531b616c583277f4cae583a90ec728b82a43
Salcetum104 <168172542+Salcetum104@users.noreply.github.com>
2024-06-06 15:53:03 +0200

    Update Jour10_Job10

--- Jour10/Job10/index.php
+++ Jour10/Job10/index.php
@@ -1,3 +1,4 @@
+
 <?php
 $mysqli = mysqli_connect("localhost", "root", "", "Jour09");
 $result = mysqli_query($mysqli, "SELECT * FROM `salles` ORDER BY capacite ASC;");
@@ -31,10 +32,12 @@ echo "</table>";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jour10_Job08</title>
+    <title>Jour10_Job10</title>
     <link rel= "stylesheet" href="./index.css">
 </head>
 <body>
     
 </body>
 </html>
+
+
